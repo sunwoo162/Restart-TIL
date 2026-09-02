45c4f58c86ff6dd332dd58da157cae1007e7e516
soonwoo <sunwoosunwoo4114@gmail.com>
2026-09-02 10:09:59 +0900

    feat : add minibank web development project

---
+++ 웹 개발/minibank/src/test/java/kr/gsm/minibank/MinibankApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gsm.minibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
